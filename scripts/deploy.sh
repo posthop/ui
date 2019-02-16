e6f1a1dca4c97dd43454393b7cc7490768441751
momothereal <momothereal.mc@gmail.com>
2019-02-16 13:55:42 -0500

    Remove hostname check

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 SSH_URI="momo@nice.momoperes.ca"
-rsync -r dist/posthop/ $SSH_URI:/etc/posthop/ui/dist/ --delete
+rsync -e "ssh -o StrictHostKeyChecking=no" -r dist/posthop/ $SSH_URI:/etc/posthop/ui/dist/ --delete
 
