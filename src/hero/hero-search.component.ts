02a2335519fcdc70c808e39c6eebc520e4e059d5
Sageewan Subendran <sageewansubendran@users.noreply.github.com>
2019-02-16 22:06:17 -0500

    UI Running

--- src/hero/hero-search.component.ts
+++ src/hero/hero-search.component.ts
@@ -1,22 +1,57 @@
-import { HttpClient, HttpErrorResponse } from '@angular/common/http';
-import { Injectable } from '@angular/core';
-import { Observable, throwError as observableThrowError } from 'rxjs';
-import { catchError } from 'rxjs/operators';
-
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Observable, Subject, of } from 'rxjs';
+import {
+  catchError,
+  debounceTime,
+  distinctUntilChanged,
+  switchMap
+} from 'rxjs/operators';
 import { Hero } from './hero';
+import { HeroSearchService } from './hero-search.service';
 
-@Injectable()
-export class HeroSearchComponent {
-  constructor(private http: HttpClient) {}
+@Component({
+  selector: 'my-hero-search',
+  templateUrl: './hero-search.component.html',
+  styleUrls: ['./hero-search.component.scss'],
+  providers: [HeroSearchService]
+})
+export class HeroSearchComponent implements OnInit {
+  heroes: Observable<Hero[]>;
+  private searchTerms = new Subject<string>();
 
-  search(term: string): Observable<Hero[]> {
-    return this.http
-        .get<Hero[]>(`app/heroes/?name=${term}`)
-        .pipe(catchError(this.handleError));
+  constructor(
+      private heroSearchService: HeroSearchService,
+      private router: Router
+  ) {}
+
+  search(term: string): void {
+    // Push a search term into the observable stream.
+    this.searchTerms.next(term);
   }
 
-  private handleError(res: HttpErrorResponse) {
-    console.error(res.error);
-    return observableThrowError(res.error || 'Server error');
+  ngOnInit(): void {
+    this.heroes = this.searchTerms.pipe(
+        debounceTime(300), // wait for 300ms pause in events
+        distinctUntilChanged(), // ignore if next search term is same as previous
+        switchMap(
+            term =>
+                term // switch to new observable each time
+                    ? // return the http search observable
+                    this.heroSearchService.search(term)
+                    : // or the observable of empty heroes if no search term
+                    of<Hero[]>([])
+        ),
+        catchError(error => {
+          // TODO: real error handling
+          console.log(`Error in component ... ${error}`);
+          return of<Hero[]>([]);
+        })
+    );
+  }
+
+  gotoDetail(hero: Hero): void {
+    const link = ['/detail', hero.id];
+    this.router.navigate(link);
   }
 }
