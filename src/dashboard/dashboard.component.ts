f709c9da4d247c04fe493213fd71824d729b562e
Sageewan Subendran <sageewansubendran@users.noreply.github.com>
2019-02-17 00:06:09 -0500

    fix

--- src/dashboard/dashboard.component.ts
+++ src/dashboard/dashboard.component.ts
@@ -1,15 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
-import { Hero } from '../hero/hero';
-
 @Component({
   selector: 'my-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
-  heroes: Hero[] = [];
 
   constructor(
       private router: Router,) {
